87b7eaa3394d1fd97c35b13a6d25ec94d8c4fdfa
Irene <irene.sebin.kim@gmail.com>
2025-12-27 11:54:46 -0800

    added event

--- restaurants.py
+++ restaurants.py
@@ -1,5 +1,5 @@
 from pyscript import document
-def on_submit():
+def on_submit(event):
     nonlocal location
     global new_url
     input_text=document.querySelector("#eng")
@@ -7,3 +7,26 @@ def on_submit():
     print("Location " + location.get())
     ##new_url = getUrlForLocation(location.get())
     ##start_search()
+
+def create_cards(dictionary, card_container, custom_font):
+    global current_card_index
+    print(current_card_index)
+    print("starting list")
+    for item in dictionary:
+        print(item["name"])
+    for item in dictionary[current_card_index:current_card_index+3]:
+        card = tk.Frame(card_container, width = 400, height = 130, bd = 2, padx = 10, pady = 10, relief = "groove")
+        card.pack_propagate(False)
+        card.pack(padx = 10, pady = 10)
+        name_ele=document.createElement('h2')
+        name_ele.innerText=item["name"]
+        label2 = tk.Label(card, text=item["location"], wraplength=220,font = custom_font)
+        label2.pack()
+        label3 = tk.Label(card, text=item["rating"],font = custom_font)
+        label3.pack()
+        label4 = tk.Label(card, text=showStars(item["rating"]),font = custom_font)
+        label4.pack()
+        card.bind("<Button-1>", lambda event, current = item: on_card_click(current["website"]))
+        for widget in card.winfo_children():
+            widget.bind("<Button-1>", lambda event, current = item: on_card_click(current["website"]))
+    current_card_index += 3
